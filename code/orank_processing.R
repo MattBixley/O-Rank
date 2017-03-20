0bff2f62dc2bb2b662f841e7ec8a555a6108a9e1
MattBixley <mattbixley72@gmail.com>
2017-03-21 08:59:29 +1300

    winsplit function

--- code/orank_processing.R
+++ code/orank_processing.R
@@ -1,28 +1,32 @@
 ### orank_processing
-source("orank_fucntions")
-statnav_db <- read.table("/home/matt/GIT_Repos/O-Rank/input/statnav_db.csv",header=T,fill=TRUE,sep=",")
+source("/home/matt/GIT_Repos/O-Rank/code/orank_functions.R")
+input <- "/home/matt/GIT_Repos/O-Rank/input/"
+output <- "/home/matt/GIT_Repos/O-Rank/output/"
+statnav_db <- read.table(paste0(input,"statnav_db.csv"),header=T,fill=TRUE,sep=",")
 
 ### read a winsplits reslut and obtain name, club, and time
 ### paste a winsplits results to result.csv and save
 
-winsplit <- read.csv("/home/matt/GIT_Repos/O-Rank/input/result.csv",header=T,fill=T)
-head(winsplit)
-rows <- seq(2,dim(winsplit)[1],by=2)
-club <- seq(3,dim(winsplit)[1],by=2)
-club <- winsplit[club,1]
+winsplit <- function(result){
+  winsplit <- read.csv(paste0(input,result),header=T,fill=T)
+  head(winsplit)
+  rows <- seq(2,dim(winsplit)[1],by=2)
+  club <- seq(3,dim(winsplit)[1],by=2)
+  club <- winsplit[club,1]
+  winsplit <- winsplit[rows,2:4]
+  time <- as.character(winsplit$Finish)
+  ltime <- lapply(time,nchar)
+  time <- cbind(time,ltime)
+  time <- ifelse(ltime<7,paste0("0:",time),time)
+  timehours <- unlist(strsplit(as.character(time),":"))[seq(1,length(time),by=2)]
+  time2 <- unlist(strsplit(as.character(time),":"))[seq(2,length(time),by=2)]
+  timemins <- unlist(substr(as.character(time2),1,2))
+  timesec <- unlist(substr(as.character(time2),4,5))
+  racetime <- round((as.numeric(timehours)*60 + as.numeric(timemins) + as.numeric(timesec)/60),2)
+  winsplit <- cbind(Name=paste0(winsplit$Name,winsplit$X),Club=club,RT=racetime)
+}
 
-winsplit <- winsplit[rows,2:4]
-winsplit$slub <- club
 
-time <- as.character(winsplit$Finish)
-ltime <- lapply(time,nchar)
-time <- cbind(time,ltime)
-
-time <- ifelse(ltime<7,paste0("0:",time),time)
-
-
-timehours <- unlist(strsplit(as.character(time),":"))
-as.Date(as.character(time))
 
 
 
