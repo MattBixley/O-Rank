805dfe893f741bf89a50933a494cdf5ed4bb7a2a
Matt Bixley <mattbixley72@gmail.com>
2017-03-21 21:01:40 +1300

    processing names and winsplits files

--- code/orank_functions.R
+++ code/orank_functions.R
@@ -1,16 +1,29 @@
 ### functions for calculating orank
 
 ### extract info from a winsplit result table
-winsplit <- function(result){
-  winsplit <- read.csv(paste0(input,result),header=T,fill=T)
-  head(winsplit)
-  rows <- seq(2,dim(winsplit)[1],by=2)
-  Name <- paste0(winsplit[rows,2],winsplit[rows,3])
-  #winsplit <- winsplit[rows,2:4]
-  time <- as.character(winsplit[rows,4])
+winsplit <- function(result,split=" "){
+  winsplit <- read.table(paste0(input,result),header=F,skip=0,fill=T,sep="\t")
+  rows <- seq(3,dim(winsplit)[1],by=2)
+  
+  ###dealing with name format
+  Name <- winsplit[rows,2]
+  ifelse(split==",",{
+    Name2 <- unlist(strsplit(as.character(Name),","))[seq(1,2*length(Name),by=2)]
+    Name1 <- unlist(strsplit(as.character(Name),","))[seq(2,2*length(Name),by=2)]
+    Name <- paste0(Name1,Name2)
+    Name <- gsub(" ","",Name)
+  },Name <- gsub(" ","",winsplit[rows,2]))
+
+  ### remove mispunches and dsq
+  time <- as.character(winsplit[rows,3])
+  x <- which(time=="mp"|time=="dsq"|time=="dnf")
+  ifelse(length(x)>0,{
+  Name <- Name[-x]
+  time <- time[-x]
+  },Name)
+  
   ltime <- lapply(time,nchar)
   time <- cbind(time,ltime)
-  
   time <- ifelse(ltime<7,paste0("0:",time),time)
   
   timehours <- unlist(strsplit(as.character(time),":"))[seq(1,2*length(time),by=2)]
@@ -22,31 +35,54 @@ winsplit <- function(result){
   return(winsplit)
 }
 
+race <- racefull
 ### statnav function
 course_score <- function(race){
-  race <- racefull
-  race$Minutes <- as.numeric(race$Minutes)
+  race <- race[order(as.numeric(as.character(race$Minutes))),]
   ## RT = Run Time
-  RT = race$Minutes
+  RT = as.numeric(as.character(race$Minutes))
   
   ## MT/ST = Mean/SD all runners time
-  MT = mean(race$Minutes)
-  ST = sd(race$Minutes)
+  MT = mean(RT)
+  ST = sd(RT)
   
   ## MP/SP = SD of ranked runners current score (mean for ALL events)
   #SP = sd of ranked runners current score) ### need a dataframe of runners
   #MP = mean points of ranked runners current score
   # bottom 10% excluded from SP and MP calc
-  
-  MP <- mean(race[,14:dim(race)[2]][1:floor(0.9*length(race$Name))],na.rm=T)
-  SP <- sd(race[,14:dim(race)[2]][1:floor(0.9*length(race$Name))],na.rm=T) 
 
-  RP = MP + (((MT-RT)/ST) * SP) 
+  MP <- mean(as.matrix(race[1:floor(0.9*length(race$Name)),15:dim(race)[2]]),na.rm=T)
+  SP <- sd(as.matrix(race[1:floor(0.9*length(race$Name)),15:dim(race)[2]]),na.rm=T)
+
+  RP <- MP + (((MT-RT)/ST) * SP) 
   
-  RankPoints <- cbind(as.character(race$Name),round(RP*5,0))
-return(RankPoints)
+  #Current Ponts to Add
+  CP <- cbind.data.frame(Name=as.character(race$Name),Points=round(RP,1))
+
+return(CP)
 }
 
 ### rescale current score points for entire database
 RPs <- 1000 + (RP - mean(RP))*200/sd(RP)
 
+### function to print my rank
+my_rank <- function(MyName){
+  statnav_db <- read.csv(paste0(input,"statnav_db.csv"),header=T,fill=TRUE,sep=";")
+  myrank <- subset(statnav_db,statnav_db$FullName==MyName,select=c("FullName","Rank","Club","Score"))
+  return(myrank)
+}
+
+rank5 <- function(x,n=5){
+  x <- x[,2:dim(x)[2]]
+  ndx <- order(x, decreasing = T)[1:n]
+  U <- sum(x[ndx])
+  return(U)
+}
+
+rank5 <- function(x,n=5){
+  # <- x[,2:dim(x)[2]]
+  ndx <- order(x, decreasing = T)[1:n]
+  U <- sum(x[ndx])
+  return(U)
+}
+  
--- code/orank_processing.R
+++ code/orank_processing.R
@@ -2,26 +2,48 @@
 source("/home/matt/GIT_Repos/O-Rank/code/orank_functions.R")
 input <- "/home/matt/GIT_Repos/O-Rank/input/"
 output <- "/home/matt/GIT_Repos/O-Rank/output/"
-statnav_db <- read.table(paste0(input,"statnav_db.csv"),header=T,fill=TRUE,sep=",")
+
+input <- "/Users/Matt/Documents/R/O_Rank/input/"
+output <- "/Users/Matt/Documents/R/O_Rank/output/"
+
+statnav_db <- read.csv(paste0(input,"statnav_db.csv"),header=T,fill=TRUE,sep=";")
+current <- statnav_db[,c(3,14:dim(statnav_db)[2])]
 
 ### read a winsplits reslut and obtain name, club, and time
 ### paste a winsplits results to result.csv and save
-result <- "result.csv"
-race <- winsplit("result.csv")
+result <- "results.txt"
 
-head(statnav_db)
-racefull <- merge(race,statnav_db,by.x="Name",by.y="FullName")
+race <- winsplit(result="results.txt",split=",")
+racefull <- merge(race,statnav_db,by.x="Name",by.y="FullName",all.x=T)
 
-head(racefull)
+### calculate the race points to add to current for ALL
+U <- course_score(racefull);U
 
-which(statnav_db$FullName %in% race$Name)
+current <- merge(current,U,by.x = "FullName",by.y="Name",all.x=T,all.y=T)
+
+write.table(current,paste0(input,"current.txt"),quote=F,col.names = F)
+
+current[current$FullName=="OllieBixley",]
+
+#current <- read.table(paste0(input,"current.txt"),header=F,row.names = 1)
+#colnames(current) <- c("FullName","C1","C2","C3","C4","C5","C6","C7","C8","C9","C10","C11","C12","C13")
+#current <- current[,-15]
+
+current[current$FullName=="OllieBixley",]
+current[current$FullName=="AnnBixley",]
+current[current$FullName=="MattBixley",]
+
+x <- current[current$FullName=="NickHann",]
+rank5(x)
+
+require(plyr)
+ddply(current, "FullName", rank5)
+
+my_rank("AnnBixley")
+my_rank("MattBixley")
 
 
-course_score(racefull)
-
-
-
-
+#### running single jobs from excel
 matarae_long <- read.table("/home/matt/GIT_Repos/O-Rank/input/matarae_long.csv",header=T,fill=TRUE,sep=",")
 mtross_c1 <- read.table("/home/matt/GIT_Repos/O-Rank/input/mtross_c1.csv",header=T,fill=TRUE,sep=",")
 matarae_medium <- read.table("/home/matt/GIT_Repos/O-Rank/input/matarae_medium.csv",header=T,fill=TRUE,sep=",")
